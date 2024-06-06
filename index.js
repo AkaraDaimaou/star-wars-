827a390ed993449905a17a05c68a32ed4600d6bc
benbeedessy15@gmail.com <benbeedessy15@gmail.com>
2024-06-06 11:54:06 +0400

    done

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
-document.getElementById('loadCharacter').addEventListener('click', loadCharacter);
+document.getElementById('fetch-button').addEventListener('click', loadCharacter);
 
 function loadCharacter() {
     const loading = document.getElementById('loading');
-    const characterdetails = document.getElementById('characterdetails');
+    const characterInfo = document.getElementById('character-info');
     const error = document.getElementById('error');
 
     loading.classList.remove('hidden');
-    characterdetails.classList.add('hidden');
+    characterInfo.classList.add('hidden');
     error.classList.add('hidden');
 
-    const characterId = math.floor(math.random()*82) + 1;
+    const characterId = Math.floor(Math.random() * 82) + 1;
     fetch(`https://www.swapi.tech/api/people/${characterId}`)
-    .then(response => response.json())
-    .then(data => {
-        const character = data.result.properties;
+        .then(response => response.json())
+        .then(data => {
+            const character = data.result.properties;
             document.getElementById('name').textContent = character.name;
             document.getElementById('height').textContent = character.height;
             document.getElementById('gender').textContent = character.gender;
-            document.getElementById('birthYear').textContent = character.birth_year;
+            document.getElementById('birth-year').textContent = character.birth_year;
 
             return fetch(character.homeworld);
         })
         .then(response => response.json())
         .then(data => {
-            document.getElementById('homeWorld').textContent = data.result.properties.name;
+            document.getElementById('home-world').textContent = data.result.properties.name;
             loading.classList.add('hidden');
             characterInfo.classList.remove('hidden');
         })
